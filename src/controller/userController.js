63a75b87c2b16d4d2ab7c97ddf766ade9c32ddaa
Felippe Keiser Voigt <felipe_k_voigt@estudante.sc.senai.br>
2024-03-21 20:58:21 -0300

    CommitPostmanV1

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,18 +1,32 @@
 const userService = require("../service/user.js");
 
 async function getAllUser(req, res) {
-try {
+  try {
     const rows = await userService.getAllUser();
 
     res.status(200).json(rows);
-} catch (error) {
+  } catch (error) {
     res.status(500).send({
-        message:"Error getting users",
-        body: error.message,
+      message: "Error getting users",
+      body: error.message,
     });
+  }
 }
+
+async function createUser(req, res) {
+  const { name, email, password } = req.body;
+  try {
+    await userService.createUser(name, email, password);
+    res.status(201).json({ message: "sucess" });
+  } catch (error) {
+    res.status(500).send({
+      message: "error ading user",
+      error: error.message,
+    });
+  }
 }
 
 module.exports = {
-    getAllUser,
+  getAllUser,
+  createUser,
 };
--- src/service/user.js
+++ src/service/user.js
@@ -2,15 +2,25 @@ const mysql = require("mysql2/promise");
 const databaseConfig = require("../config/database.js");
 
 async function getAllUser() {
-    const connection = await mysql.createConnection(databaseConfig);
+  const connection = await mysql.createConnection(databaseConfig);
 
-    const [rows] = await connection.query("SELECT * FROM user");
+  const [rows] = await connection.query("SELECT * FROM user");
 
-    await connection.end();
+  await connection.end();
 
-    return rows;
+  return rows;
+}
+
+async function createUser(name, email, password) {
+  const connection = await mysql.createConnection(databaseConfig);
+
+  const insertUser = "INSERT INTO user(name, email, password) VALUES(?, ?, ?)";
+  await connection.query(insertUser, [name, email, password]);
+
+  await connection.end();
 }
 
 module.exports = {
-    getAllUser,
+  getAllUser,
+  createUser,
 };
